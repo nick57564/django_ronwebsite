23d8a28424a82cb1896845ea827b9e5dc4e81251
nick57564 <nicksp.vanderworp@gmail.com>
2024-05-29 18:44:40 +0200

    Empty commit

--- Base/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render 
-
-def base_view(request): 
-    return render(request, "home_base.html") 
--- Fotogalerij/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render 
-from .models import MyModel
-
-def fotogalerij(request):
-    mymodels = MyModel.objects.all()
-    return render(request, 'fotogalerij_base.html', {'mymodels': mymodels})
