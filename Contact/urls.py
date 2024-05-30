a3108835c2fb9c3fb117ebc2545427e9bf48e827
Nick van der Worp <nick.van.der.worp@prodrive-technologies.com>
2024-05-30 11:05:47 +0200

    change for url

--- Contact/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import contact
-
-urlpatterns=[
-    path('', contact, name='contact'),]
--- Fotogalerij/admin.py
+++
@@ -1,7 +0,0 @@
-# myapp/admin.py
-from django.contrib import admin
-from .models import MyModel
-
-@admin.register(MyModel)
-class MyModelAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'image')
