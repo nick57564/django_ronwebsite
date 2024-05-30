48ee5e883a54be75dcc074e829f1111d6b8767d8
Nick van der Worp <nick.van.der.worp@prodrive-technologies.com>
2024-05-30 13:41:19 +0200

    prof

--- Contact/forms.py
+++ Contact/forms.py
@@ -4,10 +4,7 @@ class ContactForm(forms.Form):
     naam = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     telefoonnummer = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    adres = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    plaats = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    postcode = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    reacties = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    opmerkingen = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
 
 def send_email_task(subject, message, recipient_list, html_message=None):
     send_mail(subject, message, None, recipient_list, html_message=html_message)
